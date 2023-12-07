5a1f207b7fb5947c1eba996c3a92b17dd808a52b
Parker <parkerphillips2003@gmail.com>
2023-12-06 16:32:23 -0800

    Optimized images for web

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import styles from "./App.module.scss";
-import heroImg from "./assets/New Project.png";
+import heroImg from "./assets/heroImg.png";
 import galacticGame from "./assets/galacticgame.png";
 import galacticHome from "./assets/galacticHome.png";
+import whatSort1 from "./assets/whatsort1.jpg";
+import whatSort2 from "./assets/whatsort2.jpg";
+import portfolio1 from "./assets/portfolio1.jpg";
+import portfolio2 from "./assets/portfolio2.jpg";
 import { useState, useRef, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
@@ -32,6 +36,18 @@ function App() {
 
   return (
     <div className="App">
+      <div className={styles.loadingScreen}>
+        <div className={styles.heroImgWrapper}>
+          <img
+            alt="MERN stack loading img"
+            src={heroImg}
+            className={styles.heroImg}
+          />
+          <div className={`${styles.heroWalker}`}>
+            <div className={`${styles.walker}`}></div>
+          </div>
+        </div>
+      </div>
       <div
         className={styles.mouseGlow}
         style={{ top: mousePos.y, left: mousePos.x }}
@@ -42,25 +58,17 @@ function App() {
         }`}
       >
         <ul>
-          <li>
-            <a onClick={() => navScroll(projectsRef.current)}>Projects</a>
-          </li>
-          <li>
-            <a>Skills</a>
-          </li>
-          <li>
-            <a>Resume</a>
-          </li>
-          <li>
-            <a>Contact</a>
-          </li>
+          <li onClick={() => navScroll(projectsRef.current)}>Projects</li>
+          <li onClick={() => navScroll(skillsRef.current)}>Skills</li>
+          <li>Resume</li>
+          <li>Contact</li>
         </ul>
       </nav>
       <HeroSection />
 
       <hr className={`${styles.sectionBreak} ${styles.projectsBreak}`} />
       <div className={styles.projectSection} ref={observerRef1}>
-        <h1 ref={projectsRef} className={styles.projectsTitle} id="projects">
+        <h1 ref={projectsRef} className={styles.sectionTitle} id="projects">
           Projects
         </h1>
         <div className={styles.projectWrapper}>
@@ -70,33 +78,47 @@ function App() {
             </h2>
             <h1 className={styles.projectName}>Galactic Shooter</h1>
             <h2 className={styles.projectDesc}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium perferendis laborum fugiat amet voluptate, earum
-              voluptatum expedita rerum rem reprehenderit voluptates
-              necessitatibus dignissimos, repudiandae, cumque nulla dolor
-              obcaecati odit blanditiis.
+              2D web game that incorporates a singeplayer and multiplayer
+              gamemode. Real-time leaderboard that keeps track of the top 10
+              scores of all time. Most recent full-stack project.
             </h2>
           </div>
           <div className={styles.projectImgWrapper}>
-            <img className={styles.topImg} src={galacticHome} />
-            <img className={styles.bottomImg} src={galacticGame} />
+            <img
+              alt="Galactic Shooter home page"
+              className={styles.topImg}
+              src={galacticHome}
+            />
+            <img
+              alt="Galactic Shooter in game"
+              className={styles.bottomImg}
+              src={galacticGame}
+            />
           </div>
         </div>
 
         <div className={styles.projectWrapper}>
           <div className={styles.projectImgWrapper}>
-            <img className={styles.topImg} src={galacticHome} />
-            <img className={styles.bottomImg} src={galacticGame} />
+            <img
+              alt="array sorted by bars with stop and start buttons"
+              className={styles.topImg}
+              src={whatSort1}
+            />
+            <img
+              alt="info text about sorting algorithm"
+              className={styles.bottomImg}
+              src={whatSort2}
+            />
           </div>
           <div className={styles.projectTxt}>
             <h2 className={styles.projectTools}>
-              Tools: TypeScript, React, Express, MongoDB, Socket.io
+              Tools: React, Typescript, SCSS
             </h2>
-            <h1 className={styles.projectName}>Galactic Shooter</h1>
+            <h1 className={styles.projectName}>What The Sort</h1>
             <h2 className={styles.projectDesc}>
-              Product page for the 14th season of Destiny 2, Season of the
-              Splicer. Also the first page I ever built as a professional web
-              developer.
+              A sorting visualizer that helps students visualize different
+              sorting algorithms in Computer Science. Created for students like
+              me that can have a hard time visualizing complex ideas in class.
             </h2>
           </div>
         </div>
@@ -104,20 +126,25 @@ function App() {
         <div className={styles.projectWrapper}>
           <div className={styles.projectTxt}>
             <h2 className={styles.projectTools}>
-              Tools: TypeScript, React, Express, MongoDB, Socket.io
+              Tools: React, Typescript, SCSS
             </h2>
-            <h1 className={styles.projectName}>Galactic Shooter</h1>
+            <h1 className={styles.projectName}>My Portfolio</h1>
             <h2 className={styles.projectDesc}>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Laudantium perferendis laborum fugiat amet voluptate, earum
-              voluptatum expedita rerum rem reprehenderit voluptates
-              necessitatibus dignissimos, repudiandae, cumque nulla dolor
-              obcaecati odit blanditiis.
+              This is my most recent static Frontend project. If you'd like you
+              can check out my github repository for this page below!
             </h2>
           </div>
           <div className={styles.projectImgWrapper}>
-            <img className={styles.topImg} src={galacticHome} />
-            <img className={styles.bottomImg} src={galacticGame} />
+            <img
+              alt="Home page of portfolio"
+              className={styles.topImg}
+              src={portfolio1}
+            />
+            <img
+              alt="Projets page of portfolio"
+              className={styles.bottomImg}
+              src={portfolio2}
+            />
           </div>
         </div>
       </div>
@@ -125,7 +152,12 @@ function App() {
       <hr className={`${styles.sectionBreak} ${styles.skillsBreak}`} />
 
       <div className={styles.skillsSection} ref={observerRef2}>
-        <h1 className={styles.projectsTitle}>Skills</h1>
+        <h1
+          ref={skillsRef}
+          className={`${styles.sectionTitle} ${styles.titleDark}`}
+        >
+          Skills
+        </h1>
         <div className={styles.skillsWrapper}>
           <div className={styles.skillWrapper}>
             <h1>Frontend</h1>
@@ -143,7 +175,7 @@ function App() {
               <li>MongoDB</li>
               <li>Mongoose</li>
               <li>Socket.io</li>
-              <li></li>
+              <li>Node.js</li>
             </ul>
           </div>
           <div className={styles.skillWrapper}>
@@ -151,11 +183,16 @@ function App() {
             <ul>
               <li>Communication</li>
               <li>Timely</li>
-              <li>SCSS</li>
-              <li>Bootstrap</li>
+              <li>Problem-Solver</li>
+              <li>Attention to Detail</li>
+              <li>Motivated Learner</li>
             </ul>
           </div>
         </div>
+      </div>
+      <hr className={`${styles.sectionBreak} ${styles.contactBreak}`} />
+      <div className={styles.contactSection}>
+        <h1 className={styles.sectionTitle}>Contact</h1>
       </div>
     </div>
   );
@@ -176,7 +213,11 @@ export const HeroSection = () => {
         </a>
       </div>
       <div className={styles.heroImgWrapper}>
-        <img src={heroImg} className={styles.heroImg} />
+        <img
+          alt="MERN stack image inside bubble"
+          src={heroImg}
+          className={styles.heroImg}
+        />
         <div className={`${styles.heroWalker}`}>
           <div className={`${styles.walker}`}></div>
         </div>
